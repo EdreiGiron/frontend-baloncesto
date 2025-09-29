165d5c6ae7f06b2c457498a2f43212571786dacc
EdreiGiron <109426693+EdreiGiron@users.noreply.github.com>
2025-09-28 18:15:55 -0600

    Merge

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/auth.interceptor';
@@ -11,7 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(FormsModule)   // 👈 este es el correcto
+    importProvidersFrom(FormsModule),   // 👈 este es el correcto
     provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
